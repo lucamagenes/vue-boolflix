4f4e081693cf845ff1eaaaad52b91b193108068b
lucamagenes <lucamgns33@gmail.com>
2021-11-19 16:21:32 +0100

    install import and use fontawesome library

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import CountryFlag from 'vue-country-flag'
 
+import CountryFlag from 'vue-country-flag'
 Vue.component('country-flag', CountryFlag)
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+library.add(faFontAwesome)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 
 Vue.config.productionTip = false
